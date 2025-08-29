5795e839c006bc001ee098ac05797f5fa61191ba
wz1114841863 <18801383735@163.com>
2025-08-29 16:51:12 +0800

    add new scala files

--- tiny-gpu-chisel/playground/src/RegisterFiles.scala
+++ tiny-gpu-chisel/playground/src/RegisterFiles.scala
@@ -1,3 +1,8 @@
+//> using scala 2.13.16
+//> using dep org.chipsalliance::chisel:6.7.0
+//> using plugin org.chipsalliance:::chisel-plugin:6.7.0
+//> using options -unchecked -deprecation -language:reflectiveCalls -feature -Xcheckinit -Ymacro-annotations
+
 package registers
 
 import chisel3._
